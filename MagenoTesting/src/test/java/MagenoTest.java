dd35dd45521e3365e6a86d55e7f565e3e179eac6
ABC FOR JAVA <ABC FOR JAVA@DESKTOP-1MLER7G>
2020-01-04 09:04:37 +0000

    commit two

--- MagenoTesting/src/test/java/MagenoTest.java
+++ MagenoTesting/src/test/java/MagenoTest.java
@@ -1,7 +1,7 @@
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 public class MagenoTest {
@@ -10,7 +10,7 @@ public class MagenoTest {
 	public void testcase()
 	{
 		String url ="https://www.magento.com";
-		WebDriver driver=new ChromeDriver();
+		WebDriver driver=new FirefoxDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 		driver.get(url);
